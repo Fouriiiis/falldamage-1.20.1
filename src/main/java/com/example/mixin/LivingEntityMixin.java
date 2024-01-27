c1a571004b65ebb3f5ebedac42e45ac39f39e9c3
sawyerpatr1 <sawyerpatr1@ecs.vuw.ac.nz>
2024-01-27 22:28:00 +1300

    created project

---
+++ src/main/java/com/example/mixin/LivingEntityMixin.java
@@ -0,0 +1,22 @@
+package com.example.mixin;
+
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+
+    @Inject(method = "handleFallDamage", at = @At("HEAD"), cancellable = true)
+    private void onHandleFallDamage(float fallDistance, float damageMultiplier, DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
+
+        ((LivingEntity)(Object)this).damage(damageSource, 1);
+        cir.setReturnValue(true);
+        cir.cancel();
+    }
+}
